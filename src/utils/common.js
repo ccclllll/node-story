315c8853e763504846c7e6f650e3382a9c709755
chenlang <chenlang@eastmoney.com>
2022-01-28 10:45:57 +0800

    fix

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,10 +1,12 @@
-function inherit(child, Parent) {
-    let o = Object.create(Parent.prototype) // 以父类原型复制一个对象
-
-    child.prototype = o // 子类原型指向这个对象
-
-    o.constructor = child // 赋值构造函数，子类原型指向子类构造函数
-
+/**
+ * 继承原型
+ * @param {*} Child 
+ * @param {*} Parent 
+ */
+function inherit(Child, Parent) {
+    let o = Object.create(Parent.prototype)
+    Child.prototype = o 
+    o.constructor = Child 
 }
 
 module.exports = {
